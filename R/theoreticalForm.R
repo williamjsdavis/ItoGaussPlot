ab1d666b649129051b36f3c80e6e8f0c0a823d27
william davis <williamjsdavis@berkeley.edu>
2020-08-04 12:56:33 -0700

    Updated manuals

--- R/theoreticalForm.R
+++ R/theoreticalForm.R
@@ -1,13 +1,19 @@
-OUanalytic <- function(windowLength, gamma, D) {
-  tau_gamma <- 1 / gamma
-  wND <- windowLength / tau_gamma
-  wND3 <- wND ^ -3
-  wND4 <- wND ^ -4
-  wND5 <- wND ^ -5
-  wND6 <- wND ^ -6
-  f_wND <- wND3 - 3 * wND4 + 12 * wND6 -
-    exp(-wND) * (3 * wND4 + 12 * wND5 + 12 * wND6)
-  sqrt(24 * gamma * D * f_wND)
+OUanalytic <- function(windowLength, gamma, D, approx = "none") {
+  if (approx == "none") {
+    tau_gamma <- 1 / gamma
+    wND <- windowLength / tau_gamma
+    wND3 <- wND ^ -3
+    wND4 <- wND ^ -4
+    wND5 <- wND ^ -5
+    wND6 <- wND ^ -6
+    f_wND <- wND3 - 3 * wND4 + 12 * wND6 -
+      exp(-wND) * (3 * wND4 + 12 * wND5 + 12 * wND6)
+    sqrt(24 * gamma * D * f_wND)
+  } else if (approx == "smallW") {
+    sqrt(2.4 * D / windowLength)
+  } else {
+    print("Unrecognized approximation")
+  }
 }
 OUanalyticSmallW <-
   function(windowLength, gamma, D)
--- R/windowCalc.R
+++ R/windowCalc.R
@@ -1,18 +1,22 @@
 stdWindowSlopeCast <- function(timeIn, ampIn, windows) {
-
   # Uniform or non-uniform sampling?
   Nt <- length(timeIn)
-  equalTsteps <- diff(range(diff(timeIn))) < .Machine$double.eps ^ 0.5
+  equalTsteps <-
+    diff(range(diff(timeIn))) < .Machine$double.eps ^ 0.5
   if (equalTsteps) {
     dt <- time[2] - time[1]
-    stepHandle <- function(w) stdWindowSlopeUniform(timeIn, ampIn, w, dt, Nt)
+    stepHandle <-
+      function(w)
+        stdWindowSlopeUniform(timeIn, ampIn, w, dt, Nt)
   } else {
-    stepHandle <- function(w) stdWindowSlopeNonUniform(timeIn, ampIn, w, Nt)
+    stepHandle <-
+      function(w)
+        stdWindowSlopeNonUniform(timeIn, ampIn, w, Nt)
   }
 
   # Scalar or vector input?
   N <- length(windows)
-  if(N == 1) {
+  if (N == 1) {
     stepHandle(windows)
   } else {
     out <- windows
@@ -22,79 +26,76 @@ stdWindowSlopeCast <- function(timeIn, ampIn, windows) {
     out
   }
 }
-stdWindowSlopeUniform <- function(timeIn, ampIn, windowLength, dt, Nt) {
-  windowSteps <- floor(windowLength / dt)
-  Nwindows <- floor(Nt / windowSteps)
-  iStart <- seq(1, Nt, windowSteps)
+stdWindowSlopeUniform <-
+  function(timeIn, ampIn, windowLength, dt, Nt) {
+    windowSteps <- floor(windowLength / dt)
+    Nwindows <- floor(Nt / windowSteps)
+    iStart <- seq(1, Nt, windowSteps)
 
-  n <- windowSteps
-  N <- Nwindows
-  sum <- 0
-  sumSq <- 0
+    n <- windowSteps
+    N <- Nwindows
+    sum <- 0
+    sumSq <- 0
 
-  for (i in 1:Nwindows) {
-    ii <- iStart[i]
-    sumX <- 0
-    sumY <- 0
-    sumXY <- 0
-    sumXX <- 0
-    for (j in 1:windowSteps) {
-      sumX <- sumX + timeIn[ii]
-      sumY <- sumY + ampIn[ii]
-      sumXY <- sumXY + timeIn[ii] * ampIn[ii]
-      sumXX <- sumXX + timeIn[ii] * timeIn[ii]
-      ii <- ii + 1
+    for (i in 1:Nwindows) {
+      ii <- iStart[i]
+      sumX <- 0
+      sumY <- 0
+      sumXY <- 0
+      sumXX <- 0
+      for (j in 1:windowSteps) {
+        sumX <- sumX + timeIn[ii]
+        sumY <- sumY + ampIn[ii]
+        sumXY <- sumXY + timeIn[ii] * ampIn[ii]
+        sumXX <- sumXX + timeIn[ii] * timeIn[ii]
+        ii <- ii + 1
+      }
+      # Slope of windowed section
+      B <- (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX)
+
+      sum <- sum + B
+      sumSq <- sumSq + B * B
     }
-
-    # Slope of windowed section
-    B <- (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX)
-
-    sum <- sum + B
-    sumSq <- sumSq + B * B
+    # Variance of all slopes
+    varB <- (sumSq - (sum * sum) / N) / (N - 1)
+    sqrt(varB)
   }
+stdWindowSlopeNonUniform <-
+  function(timeIn, ampIn, windowLength, Nt) {
+    Nwindows = floor(tail(timeIn, n = 1) / windowLength)
 
-  # Variance of all slopes
-  varB <- (sumSq - (sum * sum) / N) / (N - 1)
-  sqrt(varB)
-}
-stdWindowSlopeNonUniform <- function(timeIn, ampIn, windowLength, Nt) {
-  Nwindows = floor(tail(timeIn, n=1)/windowLength)
+    tStart <- 0
+    iStart <- 1L
+    iEnd <- findInterval(windowLength, timeIn)
 
-  tStart <- 0
-  iStart <- 1L
-  iEnd <- findInterval(windowLength, timeIn)
+    N <- Nwindows
+    sum <- 0
+    sumSq <- 0
+    for (i in 1:Nwindows) {
+      n <- iEnd - iStart
+      ii <- iStart
+      sumX <- 0
+      sumY <- 0
+      sumXY <- 0
+      sumXX <- 0
+      for (j in 1:n) {
+        sumX <- sumX + timeIn[ii]
+        sumY <- sumY + ampIn[ii]
+        sumXY <- sumXY + timeIn[ii] * ampIn[ii]
+        sumXX <- sumXX + timeIn[ii] * timeIn[ii]
+        ii <- ii + 1
+      }
+      # Slope of windowed section
+      B <- (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX)
 
-  N <- Nwindows
-  sum <- 0
-  sumSq <- 0
-  for (i in 1:Nwindows) {
-    n <- iEnd - iStart
-    ii <- iStart
-    sumX <- 0
-    sumY <- 0
-    sumXY <- 0
-    sumXX <- 0
-    for (j in 1:n) {
-      sumX <- sumX + timeIn[ii]
-      sumY <- sumY + ampIn[ii]
-      sumXY <- sumXY + timeIn[ii] * ampIn[ii]
-      sumXX <- sumXX + timeIn[ii] * timeIn[ii]
-      ii <- ii + 1
+      sum <- sum + B
+      sumSq <- sumSq + B * B
+
+      tStart <- tStart + windowLength
+      iStart <- iEnd
+      iEnd <- findInterval(tStart + windowLength, timeIn)
     }
-
-    # Slope of windowed section
-    B <- (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX)
-
-    sum <- sum + B
-    sumSq <- sumSq + B * B
-
-    tStart <- tStart + windowLength
-    iStart <- iEnd
-    iEnd <- findInterval(tStart + windowLength, timeIn)
+    # Variance of all slopes
+    varB <- (sumSq - (sum * sum) / N) / (N - 1)
+    sqrt(varB)
   }
-
-  # Variance of all slopes
-  varB <- (sumSq - (sum * sum) / N) / (N - 1)
-  sqrt(varB)
-
-}
