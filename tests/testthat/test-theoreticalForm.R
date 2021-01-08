6438b76fae267cb37eaf6d3617ae38086c6f323f
william davis <williamjsdavis@berkeley.edu>
2021-01-08 12:03:09 -0800

    Added more tests

--- tests/testthat/test-theoreticalForm.R
+++ tests/testthat/test-theoreticalForm.R
@@ -1,6 +1,7 @@
 test_that("Scalar output", {
   out <- OUanalytic(0.1,1,1)
   expect_equal(length(out), 1L)
+
   out <- OUanalytic(0.1,1.5,2)
   expect_equal(length(out), 1L)
 })
@@ -8,6 +9,7 @@ test_that("Vector output", {
   N <- 5L
   out <- OUanalytic(10^seq(-1,2,len=N),1,1)
   expect_equal(length(out), N)
+
   N <- 10L
   out <- OUanalytic(10^seq(-1,2,len=N),1.5,2)
   expect_equal(length(out), N)
@@ -15,6 +17,7 @@ test_that("Vector output", {
 test_that("Scalar output, small w approx.", {
   out <- OUanalyticSmallW(0.1,1,1)
   expect_equal(length(out), 1L)
+
   out <- OUanalyticSmallW(0.1,1.5,2)
   expect_equal(length(out), 1L)
 })
@@ -22,6 +25,7 @@ test_that("Vector output, small w approx.", {
   N <- 5L
   out <- OUanalyticSmallW(10^seq(-1,2,len=N),1,1)
   expect_equal(length(out), N)
+
   N <- 10L
   out <- OUanalyticSmallW(10^seq(-1,2,len=N),1.5,2)
   expect_equal(length(out), N)
--- tests/testthat/test-windowCalc.R
+++ tests/testthat/test-windowCalc.R
@@ -3,12 +3,23 @@ test_that("Uniform time, scalar output", {
   amplitude <- sin(timeU)
   out <- stdWindowSlopeCast(timeU, amplitude, 0.1)
   expect_equal(length(out), 1L)
+
+  timeU <- seq(1, 20, len = 2000)
+  amplitude <- sin(timeU)
+  out <- stdWindowSlopeCast(timeU, amplitude, 0.2)
+  expect_equal(length(out), 1L)
 })
 test_that("Uniform time, vector output", {
   timeU <- seq(0, 10, len = 1000)
   amplitude <- sin(timeU)
   N <- 5L
   out <- stdWindowSlopeCast(timeU, amplitude, seq(0.1, 0.8, len = N))
+  expect_equal(length(out), N)
+
+  timeU <- seq(1, 20, len = 2000)
+  amplitude <- sin(timeU)
+  N <- 10L
+  out <- stdWindowSlopeCast(timeU, amplitude, seq(0.2, 0.9, len = N))
   expect_equal(length(out), N)
 })
 test_that("Non-uniform time, scalar output", {
@@ -17,6 +28,12 @@ test_that("Non-uniform time, scalar output", {
   amplitude <- sin(timeExp)
   out <- stdWindowSlopeCast(timeExp, amplitude, 10)
   expect_equal(length(out), 1L)
+
+  timeU <- seq(1, 20, len = 2000)
+  timeExp <- 0.001 * exp(timeU)
+  amplitude <- sin(timeExp)
+  out <- stdWindowSlopeCast(timeExp, amplitude, 20)
+  expect_equal(length(out), 1L)
 })
 test_that("Non-uniform time, vector output", {
   timeU <- seq(0, 10, len = 1000)
@@ -24,5 +41,12 @@ test_that("Non-uniform time, vector output", {
   amplitude <- sin(timeExp)
   N <- 5L
   out <- stdWindowSlopeCast(timeExp, amplitude, seq(0.1, 0.8, len = N))
+  expect_equal(length(out), N)
+
+  timeU <- seq(1, 20, len = 2000)
+  timeExp <- 0.001 * exp(timeU)
+  amplitude <- sin(timeExp)
+  N <- 10L
+  out <- stdWindowSlopeCast(timeExp, amplitude, seq(0.2, 0.9, len = N))
   expect_equal(length(out), N)
 })
